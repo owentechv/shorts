03e86599eca1d7451497b097cdb04bbcdb3f9605
x83owen <86007920+x83owen@users.noreply.github.com>
2024-05-23 19:48:41 -0400

    Create redirect.js

---
+++ mobile/redirect.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    if (/Windows/i.test(navigator.userAgent)) {
+        window.location.href = "https://owentechv.github.io/shorts"; // Redirect to mobile version
+    }
+});
