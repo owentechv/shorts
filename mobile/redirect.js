41bee75ad11eba476ab8be0bb34e633bb9ad715e
x83owen <86007920+x83owen@users.noreply.github.com>
2024-05-24 11:08:16 -0400

    Delete mobile/redirect.js

--- mobile/redirect.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if (/Windows/i.test(navigator.userAgent)) {
-        window.location.href = "https://owentechv.github.io/shorts"; // Redirect to mobile version
-    }
-});
